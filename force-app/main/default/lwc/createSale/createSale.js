f34d57cad3ac2a4363ef74c7eb3334c35b9e3011
Vaibhav Kumar <vaibhavnehwal@gmail.com>
2023-05-22 00:04:37 +0530

    Items&Invoice Realtion Maintain

--- force-app/main/default/lwc/createSale/createSale.js
+++ force-app/main/default/lwc/createSale/createSale.js
@@ -9,6 +9,8 @@ import DISTRICT_FIELD from '@salesforce/schema/Sale_Invoice__c.District__c';
 import UoM_FIELD from '@salesforce/schema/Product_Item__c.UoM__c';
 import STATE_FIELD from '@salesforce/schema/Sale_Invoice__c.State__c';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import createRecordApex from '@salesforce/apex/CreateINvRecord.createRecord';
+
 import { getRecord } from 'lightning/uiRecordApi';
 import  getStateDistrictPicklist from '@salesforce/apex/StateDistrictPicklist.getStateDistrictPicklist';
 const FIELDS = ['Sale_Invoice__c.Party__r.Name', 'Product_Item__c.Product2.Name'];
@@ -16,6 +18,7 @@ const FIELDS = ['Sale_Invoice__c.Party__r.Name', 'Product_Item__c.Product2.Name'
 
 export default class CreateSale extends LightningElement {
   //Test Comment 12
+  @api InvData ={Party:"", InvNumber:"", Contact:"", Invdate:"", Phone:"", Email:"", Village:"", Block:"", Taishil:"", State:"", District:""}
   FortuneLogoUrl = Logo;
   addIcon=ADDICON;
   DeleteIcon=DELETEICON;
@@ -56,7 +59,7 @@ get formattedDiscount() {
       }
   }
 
-  handleStateChange(event) {
+ /* handleStateChange(event) {
       this.selectedState = event.detail.value;
       this.selectedDistrict = '';
       this.isDistrictDisabled = true;
@@ -77,7 +80,7 @@ get formattedDiscount() {
          // });
         //  this.isDistrictDisabled = false;
       }
-  }
+  }   */
 
   ratehandler(event) {
   
@@ -132,13 +135,40 @@ discounthandler(event)
 
 
         handleAccountSelection(event){
-            this.selectedAccount = event.target.value;
-            alert("The selected Accout id is"+this.selectedAccount);
+            this.InvData.Party = event.target.value;
+            
         }
         HandleFocus(){
-            if(this.selectedAccount==""){ 
+            if(this.InvData.Party==""){ 
                     alert("Please Select Party First");
             }
+        }
+        InvChnageHandler(event){
+          this.InvData.InvNumber=event.target.value;
+        }
+        ContacChangetHandler(event){
+          this.InvData.Contact=event.target.value;
+        }
+        InvDateHandler(event){
+          this.InvData.Invdate=event.target.value;
+        }
+        PhoneChangeHandler(event){
+          this.InvData.Phone=event.target.value;
+        }
+        EmailChangeHandler(event){
+         this.InvData.Email=event.target.value;
+        }
+        VillageHandler(event){
+          this.InvData.Village=event.target.value;
+        }
+        BlockHandler(event){
+          this.InvData.Block=event.target.value;
+        }
+        taisilHanlder(event){
+          this.InvData.Taishil=event.target.value;
+        }
+        DistrictHandler(event){
+         this.InvData.District=event.target.value;
         }
 @wire(getObjectInfo, { objectApiName: SALE_INVOICE_OBJECT })
     SaleInvoiceInfo;
@@ -189,4 +219,78 @@ uomPicklistValues({ error, data }) {
     this.rows.push({ SrNo:rowNumber, id:this.nextId,item: '', description: '',  quantity: 0, UoM: '',  rate: 0, Discount: 0,DiscountPrice: 0, Total: 0 });
     
   } 
+
+
+  stateVSdistrict = { 
+    "Uttar Pradesh" : [{value: 'Aligarh', label: 'Aligarh'}, {value: 'Bulandsahar', label: 'Bulandsahar'} ],
+    "Madhya Pradesh" : [{value: 'Raisen', label: 'Raisen'}, {value: 'Bhopal', label: 'Bhopal'}]
+}
+
+DistrictValues = [
+    { value: 'Aligarh', label: 'Aligarh', description: 'A new item' },
+    {
+        value: 'Raisen',
+        label: 'Raisen'
+        
+    }
+];
+
+value = 'new';
+
+
+userInput ;
+filertoption;
+ handleInvoiceBlur = (event)=> {
+ 
+ this.userInput = event.target.value;
+ this.InvData.State=event.target.value;
+    console.log(this.userInput);
+
+    //this.filertoption = stateVSdistrict["Uttar Pradesh"];
+    //this.filertoption = this.stateVSdistrict['"' + this.userInput + '"'];
+     this.filertoption = this.stateVSdistrict[this.userInput ];
+    console.log(this.filertoption);   
+}
+
+DeleteHandler(event){
+  const itemId = event.target.dataset.id;
+  console.log('itemId@@@'+itemId);
+  const elementToRemove = this.rows.find(row => row.id == itemId);
+  console.log( 'elementToRemove@@'+elementToRemove);
+  var OldRows=[...this.rows]
+  console.log(OldRows);
+  this.rows = OldRows.filter(item => item !== elementToRemove);
+  console.log( this.rows);
+}
+
+handleSubmit(){
+  const jsonString = JSON.stringify(this.InvData);
+  const JsonStr=JSON.stringify(this.rows);
+  //const RowData=this.rows;
+  console.log("Items@@"+JsonStr);
+  console.log(jsonString);
+  var parentId;
+  createRecordApex({ jsonString , JsonStr})
+            .then(result => {
+                // Record creation successful
+                console.log('Record created with ID'+result);
+                parentId=result;
+                // Handle success or perform any additional operations
+                //return createChildRecord({RowData, parentId })
+            })
+            
+   //const Items = JSON.stringify(this.rows);
+  
+  //.then(result2 => {
+    // Record creation successful
+    //console.log('Child created with ID');
+    
+    // Handle success or perform any additional operations
+//})
+.catch(error => {
+    // Record creation failed
+    console.error('Error creating child record:', error);
+    // Handle error or display error message
+});
+}
 }
